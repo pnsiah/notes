5d4d1bbf98ee18e2d7047f90eec28ac810f1f2ab
pnsiah <pascal.live00@gmail.com>
2025-10-21 15:33:44 +0000

    Update url

--- notes/api/urls.py
+++ notes/api/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("api/create_folder/", views.create_folder, name="create_folder"),
     path("api/dashboard", views.dashboard, name="dashboard"),
     path("api/delete_note/<int:note_id>/", views.delete_note, name="delete_note"),
+    path("api/update_note/<int:note_id>/", views.update_note, name="update_note"),
     path("api/create_note/", views.create_note, name="create_note"),
     path("api/message/", views.message),
 ]
