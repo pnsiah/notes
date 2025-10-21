ee0168b8d5c1b67dff1d8cc6eb8d8cacd92c2fbc
pnsiah <pascal.live00@gmail.com>
2025-10-21 16:13:37 +0000

    Added listing tags

--- notes/api/urls.py
+++ notes/api/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path("api/logout/", views.logout_view, name="logout"),
     path("api/create_folder/", views.create_folder, name="create_folder"),
     path("api/dashboard", views.dashboard, name="dashboard"),
+    path(
+        "api/list_archived_notes", views.list_archived_notes, name="list_archived_notes"
+    ),
     path("api/delete_note/<int:note_id>/", views.delete_note, name="delete_note"),
     path("api/update_note/<int:note_id>/", views.update_note, name="update_note"),
     path("api/create_note/", views.create_note, name="create_note"),
