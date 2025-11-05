100958df68ba1f81a20ae3a266598cf397384006
pnsiah <pascal.live00@gmail.com>
2025-11-05 10:50:32 +0000

    Styling taglist

--- notes/frontend/src/components/TagList.jsx
+++ notes/frontend/src/components/TagList.jsx
@@ -1,18 +1,21 @@
 import tag from "../assets/images/icon-tag.svg";
+import "../components/TagList.css";
 function TagList() {
   return (
     <section className="tags">
-      <h3 className="tags-heading">Tags</h3>
-      <p className="tag">
-        <img src={tag} alt="" /> <span className="tag-text">Tags</span>
-      </p>
+      <h3 className="section-heading">Tags</h3>
+      <ul className="tag-list">
+        <li className="tag">
+          <img src={tag} alt="" /> <span className="tag-text">Tags</span>
+        </li>
 
-      <p className="tag">
-        <img src={tag} alt="" /> <span className="tag-text">Element</span>
-      </p>
-      <p className="tag">
-        <img src={tag} alt="" /> <span className="tag-text">React</span>
-      </p>
+        <li className="tag">
+          <img src={tag} alt="" /> <span className="tag-text">Element</span>
+        </li>
+        <li className="tag">
+          <img src={tag} alt="" /> <span className="tag-text">React</span>
+        </li>
+      </ul>
     </section>
   );
 }
