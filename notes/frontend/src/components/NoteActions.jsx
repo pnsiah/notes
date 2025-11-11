4d42cd65bd460b0ab0c7144c93a9c1e78eb128ff
pnsiah <pascal.live00@gmail.com>
2025-11-11 23:41:54 +0000

    Styled Frontend

--- notes/frontend/src/components/NoteActions.jsx
+++ notes/frontend/src/components/NoteActions.jsx
@@ -1,16 +1,17 @@
 import deleteIcon from "../assets/images/icon-delete.svg";
 import archive from "../assets/images/icon-archive.svg";
+import "../components/NoteActions.css";
 
 function NoteActions() {
   return (
     <div className="note-actions">
-      <button className="danger action-button">
-        <img src={deleteIcon} alt="" />
-        <span>Delete Note </span>
-      </button>
       <button className="action-button">
-        <img src={archive} alt="" />
+        <img className="icon" src={archive} alt="" />
         <span>Archive Note</span>{" "}
+      </button>
+      <button className="danger action-button">
+        <img className="icon" src={deleteIcon} alt="" />
+        <span>Delete Note </span>
       </button>
     </div>
   );
