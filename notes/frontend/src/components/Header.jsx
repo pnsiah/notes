aaf9c7ef08e33ec2170b5eb1ff1485160e40b518
pnsiah <pascal.live00@gmail.com>
2025-12-28 14:30:01 +0000

    header comments

--- notes/frontend/src/components/Header.jsx
+++ notes/frontend/src/components/Header.jsx
@@ -2,17 +2,7 @@ import Search from "./Search";
 import Logo from "./Logo";
 import "../components/Header.css";
 
-function Header({
-  heading,
-  // setSearchQuery,
-  // searchQuery,
-  // fetchNotes,
-  // setEmptyState,
-  // setNotesInfoMessage,
-  // searchNotes,
-  // showSearch = true,
-  // showLogo = false,
-}) {
+function Header({ heading }) {
   return (
     <header className="header">
       <h2 className="header-text">{heading}</h2>
