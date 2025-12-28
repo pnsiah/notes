d5d5bc232828b3642b7140103072385cb75805c7
pnsiah <pascal.live00@gmail.com>
2025-12-28 14:46:22 +0000

    navbar comments

--- notes/frontend/src/components/NavBar.jsx
+++ notes/frontend/src/components/NavBar.jsx
@@ -3,7 +3,6 @@ import home from "../assets/images/icon-home.svg";
 import archive from "../assets/images/icon-archive.svg";
 import tagIcon from "../assets/images/icon-tag.svg";
 import search from "../assets/images/icon-search.svg";
-import settings from "../assets/images/icon-settings.svg";
 import folderIcon from "../assets/images/folder-open-regular-full.svg";
 import "../components/NavBar.css";
 
@@ -13,7 +12,6 @@ function NavBar({ view, setView, setHeading }) {
     { id: "archived", icon: archive },
     { id: "tags", icon: tagIcon },
     { id: "search", icon: search },
-    // { id: "settings", icon: settings },
     { id: "folders", icon: folderIcon },
   ];
 
