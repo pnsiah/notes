48a04dd0d7067b453df52ca25799d845a337babd
pnsiah <pascal.live00@gmail.com>
2025-12-29 16:54:48 +0000

    empty list message

--- notes/frontend/src/components/FolderList.jsx
+++ notes/frontend/src/components/FolderList.jsx
@@ -23,15 +23,21 @@ function FolderList({
     <section className="folders">
       <h3 className="section-heading">Folders</h3>
       <ul className="folder-list">
-        {folders.map((folder) => (
-          <li key={folder.id} onClick={() => fetchFolderNotes(folder)}>
-            <button className="folder">
-              <img src={folderIcon} alt="" />
-              <span className="tag-text">{folder.name}</span>
-              <img className="arrow-right" src={arrow_right} alt="" />
-            </button>
+        {folders.length === 0 ? (
+          <li className="empty-folder-message">
+            No folders available. Create a new folder to get started.
           </li>
-        ))}
+        ) : (
+          folders.map((folder) => (
+            <li key={folder.id} onClick={() => fetchFolderNotes(folder)}>
+              <button className="folder">
+                <img src={folderIcon} alt="" />
+                <span className="tag-text">{folder.name}</span>
+                <img className="arrow-right" src={arrow_right} alt="" />
+              </button>
+            </li>
+          ))
+        )}
       </ul>
     </section>
   );
--- notes/frontend/src/components/TagList.jsx
+++ notes/frontend/src/components/TagList.jsx
@@ -16,25 +16,31 @@ function TagList({
     <section className="tags">
       <h3 className="section-heading">Tags</h3>
       <ul className="tag-list">
-        {tags.map((tag) => (
-          <li
-            key={tag.id}
-            onClick={() => {
-              setSelectedTag({ id: tag.id, name: tag.name });
-              setView("taggedNotes");
-              setHighlight("");
-              setSelectedFilter("taggedNotes");
-              setHeading(`Tagged Notes: ${tag.name}`);
-              // setSelectedFilter(null);
-            }}
-          >
-            <button className="tag">
-              <img src={tagIcon} alt="" />
-              <span className="tag-text">{tag.name}</span>
-              <img className="arrow-right" src={arrow_right} alt="" />
-            </button>
+        {tags.length === 0 ? (
+          <li className="empty-tag-message">
+            No tags available. Add some tags to organize your notes!
           </li>
-        ))}
+        ) : (
+          tags.map((tag) => (
+            <li
+              key={tag.id}
+              onClick={() => {
+                setSelectedTag({ id: tag.id, name: tag.name });
+                setView("taggedNotes");
+                setHighlight("");
+                setSelectedFilter("taggedNotes");
+                setHeading(`Tagged Notes: ${tag.name}`);
+                // setSelectedFilter(null);
+              }}
+            >
+              <button className="tag">
+                <img src={tagIcon} alt="" />
+                <span className="tag-text">{tag.name}</span>
+                <img className="arrow-right" src={arrow_right} alt="" />
+              </button>
+            </li>
+          ))
+        )}
       </ul>
     </section>
   );
